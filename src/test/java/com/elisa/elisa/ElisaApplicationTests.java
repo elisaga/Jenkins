10c99357de9a09125fc1bbd11fa9df2cec8cdbef
elisaga <135338486+elisaga@users.noreply.github.com>
2023-06-07 09:39:50 +0200

    Add files via upload

---
+++ src/test/java/com/elisa/elisa/ElisaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elisa.elisa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElisaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
